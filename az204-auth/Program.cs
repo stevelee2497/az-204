a107debc6291259ad45a4304294c8dcbbec1754d
Quoc Tran <quoc.tran@saigontechnology.com>
2023-06-19 10:35:04 +0700

    Az-204 practices

---
+++ az204-auth/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+const string _clientId = "468e8097-c0fa-4a3f-afdc-f1ce8d1742fa";
+const string _tenantId = "887ae08b-33e7-4a86-a59e-ef30765fb606";
+
+var app = PublicClientApplicationBuilder
+  .Create(_clientId)
+  .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+  .WithRedirectUri("http://localhost")
+  .Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
---
+++ az204-redis-cache/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string connectionString = "az204redis30473.redis.cache.windows.net:6380,password=ILUtP3U3AlXCMegT9jQNP0iOEELCE9z4LAzCaNVrFFA=,ssl=True,abortConnect=False";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase db = cache.GetDatabase();
+
+    // Snippet below executes a PING to test the server connection
+    var result = await db.ExecuteAsync("ping");
+    Console.WriteLine($"PING = {result.Type} : {result}");
+
+    // Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+    bool setValue = await db.StringSetAsync("test:key", "100");
+    Console.WriteLine($"SET: {setValue}");
+
+    // StringGetAsync retrieves the value for the "test" key
+    var getValue = await db.StringGetAsync("test:key");
+    Console.WriteLine($"GET: {getValue}");
+}
